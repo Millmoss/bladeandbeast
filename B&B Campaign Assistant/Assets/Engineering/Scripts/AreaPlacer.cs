7c889928259f2d2040b3fe6992e0c826d85c016d
Millmoss <omelliff@yahoo.com>
2018-04-09 02:43:34 -0400

    FUCKING STRINGS

--- B&B Campaign Assistant/Assets/Engineering/Scripts/AreaPlacer.cs
+++ B&B Campaign Assistant/Assets/Engineering/Scripts/AreaPlacer.cs
@@ -11,50 +11,48 @@ public class AreaPlacer : MonoBehaviour
 
 	void Start ()
 	{
-		areaMap = new Vector3[100, 100];
-		for (int z = 0; z < 100; z++)
+		int polyscale = 10;
+		int sizescale = 10;
+		float perlinscale = .75f;
+		areaMap = new Vector3[polyscale, polyscale];
+		for (int z = 0; z < polyscale; z++)
 		{
-			for (int x = 0; x < 100; x++)
+			for (int x = 0; x < polyscale; x++)
 			{
-				//areaMap[z, x] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-				//areaMap[z, x].transform.localScale = new Vector3(.1f, .5f, .1f);
-				//Destroy(areaMap[z, x].GetComponent<BoxCollider>());
-				areaMap[z, x] = new Vector3(.1f * x, Mathf.PerlinNoise(x * .1f, z * .1f), .1f * z);
-				//float r = Random.value;
-				//Renderer change = areaMap[z, x].GetComponent<Renderer>();
-				//change.material = blue;
+				areaMap[z, x] = new Vector3(.1f * x * sizescale, Mathf.PerlinNoise(x * perlinscale, z * perlinscale), .1f * z * sizescale);
 			}
 		}
 		GameObject r = new GameObject("Plane");
 		r.AddComponent<MeshFilter>();
 		r.AddComponent<MeshRenderer>();
 		r.GetComponent<MeshRenderer>().material = blue;
-		Vector3[] vertexList = new Vector3[10000];
-		int[] triangleList = new int[58806];
+		Vector3[] vertexList = new Vector3[polyscale * polyscale];
+		int[] triangleList = new int[(polyscale - 1) * (polyscale - 1) * 6];
 
-		for (int z = 0; z < 100; z++)
+		for (int z = 0; z < polyscale; z++)
 		{
-			for (int x = 0; x < 100; x++)
+			for (int x = 0; x < polyscale; x++)
 			{
-				vertexList[z * 100 + x] = areaMap[x, z];
+				vertexList[z * polyscale + x] = areaMap[x, z];
 			}
 		}
 		int cur = 0;
-		for (int z = 0; z < 99; z++)
+		for (int z = 0; z < polyscale - 1; z++)
 		{
-			for (int x = 0; x < 99; x++)
+			for (int x = 0; x < polyscale - 1; x++)
 			{
-				triangleList[cur + 2] = z * 100 + x;
-				triangleList[cur + 1] = (z + 1) * 100 + x;
-				triangleList[cur + 0] = z * 100 + x + 1;
-				triangleList[cur + 3] = (z + 1) * 100 + x;
-				triangleList[cur + 4] = z * 100 + x + 1;
-				triangleList[cur + 5] = (z + 1) * 100 + x + 1;
+				triangleList[cur + 2] = z * polyscale + x;
+				triangleList[cur + 1] = (z + 1) * polyscale + x;
+				triangleList[cur + 0] = z * polyscale + x + 1;
+				triangleList[cur + 3] = (z + 1) * polyscale + x;
+				triangleList[cur + 4] = z * polyscale + x + 1;
+				triangleList[cur + 5] = (z + 1) * polyscale + x + 1;
 				cur += 6;
 			}
 		}
 		r.GetComponent<MeshFilter>().mesh.vertices = vertexList;
 		r.GetComponent<MeshFilter>().mesh.triangles = triangleList;
+		r.GetComponent<MeshFilter>().mesh.RecalculateNormals();
 	}
 	
 	void Update ()
@@ -62,3 +60,6 @@ public class AreaPlacer : MonoBehaviour
 
 	}
 }
+
+//Renderer change = areaMap[z, x].GetComponent<Renderer>();
+//change.material = blue;
---
+++ B&B Campaign Assistant/Assets/Engineering/Scripts/GameManagerImporter.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class GameManagerImporter : MonoBehaviour
+{
+	//IMPORTS
+
+	//imported character initialization method
+	[DllImport("GameManager")] public static extern void createCharacter();
+
+	//imported set methods
+	[DllImport("GameManager")] public static extern int setName(int index, string name);
+	[DllImport("GameManager")] public static extern int setStrength(int index, int s);
+	[DllImport("GameManager")] public static extern int setDexterity(int index, int d);
+	[DllImport("GameManager")] public static extern int setAgility(int index, int a);
+	[DllImport("GameManager")] public static extern int setConstitution(int index, int c);
+	[DllImport("GameManager")] public static extern int setIntellect(int index, int i);
+	[DllImport("GameManager")] public static extern int setWillpower(int index, int w);
+	[DllImport("GameManager")] public static extern int setPerception(int index, int p);
+	[DllImport("GameManager")] public static extern int setCharisma(int index, int c);
+	[DllImport("GameManager")] public static extern int setBeauty(int index, int b);
+	[DllImport("GameManager")] public static extern int setBirth(int index, int y, int d, int s);
+	[DllImport("GameManager")] public static extern int setBaseHeight(int index, float h);
+	[DllImport("GameManager")] public static extern int setBaseWeight(int index, float w);
+
+	//imported get methods
+	[DllImport("GameManager")] public static extern System.IntPtr getName(int index);
+	[DllImport("GameManager")] public static extern int getStrength(int index);
+	[DllImport("GameManager")] public static extern int getDexterity(int index);
+	[DllImport("GameManager")] public static extern int getAgility(int index);
+	[DllImport("GameManager")] public static extern int getConstitution(int index);
+	[DllImport("GameManager")] public static extern int getIntellect(int index);
+	[DllImport("GameManager")] public static extern int getWillpower(int index);
+	[DllImport("GameManager")] public static extern int getPerception(int index);
+	[DllImport("GameManager")] public static extern int getCharisma(int index);
+	[DllImport("GameManager")] public static extern int getBeauty(int index);
+	[DllImport("GameManager")] public static extern System.IntPtr getAge(int index);
+
+	[DllImport("GameManager")] public static extern int getRestrictMax(int index);
+	[DllImport("GameManager")] public static extern int getRestrict(int index);
+	[DllImport("GameManager")] public static extern int getOverheatMax(int index);
+	[DllImport("GameManager")] public static extern int getOverheat(int index);
+	[DllImport("GameManager")] public static extern int getDamageBonus(int index);
+	[DllImport("GameManager")] public static extern int getCarryMax(int index);
+	[DllImport("GameManager")] public static extern float getCarry(int index);
+	[DllImport("GameManager")] public static extern int getLiftMax(int index);
+	[DllImport("GameManager")] public static extern int getWeaponProficiency(int index);
+	[DllImport("GameManager")] public static extern int getInitiativeBase(int index);
+	[DllImport("GameManager")] public static extern int getInitiative(int index);
+	[DllImport("GameManager")] public static extern int getDefenseBase(int index);
+	[DllImport("GameManager")] public static extern int getDefense(int index);
+	[DllImport("GameManager")] public static extern int getHealthMax(int index);
+	[DllImport("GameManager")] public static extern float getHealth(int index);
+	[DllImport("GameManager")] public static extern float getHealSpeed(int index);
+	[DllImport("GameManager")] public static extern float getBaseWeight(int index);
+	[DllImport("GameManager")] public static extern float getBaseHeight(int index);
+	[DllImport("GameManager")] public static extern float getWeight(int index);
+	[DllImport("GameManager")] public static extern float getHeight(int index);
+	[DllImport("GameManager")] public static extern int getResolve(int index);
+	[DllImport("GameManager")] public static extern int getSorceryBonus(int index);
+	[DllImport("GameManager")] public static extern int getEyeglassesBonus(int index);
+	[DllImport("GameManager")] public static extern System.IntPtr getEyesight(int index);
+
+	//LOCAL VARIABLES
+
+	int characterCount;
+
+	// Use this for initialization
+	void Start () {
+		createCharacter();
+		characterCount++;
+		setStrength(0, 10);
+		setName(0, "BOBERT");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print(getStrength(0));
+		print(Marshal.PtrToStringAnsi(getName(0), 6));
+	}
+}
