fc21a8a1a0afcdb6246fbafa1f968da9d7f06bd9
Millmoss <omelliff@yahoo.com>
2018-05-07 00:35:57 -0400

    Selecting and deselecting a token has been added. Once selected the gameObject is stored in script and can be moved and stuff.

---
+++ B&B Campaign Assistant/Assets/Engineering/Scripts/InteractToken.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractToken : MonoBehaviour
+{
+	public Camera unitCamera;
+	public GameObject devPointer;
+	private GameObject selectedObject;
+
+	void Start ()
+	{
+		selectedObject = null;
+	}
+	
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = unitCamera.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+			LayerMask selectionMask = 1 << 8;
+			RaycastHit hitObject = new RaycastHit();
+			if (Physics.Raycast(ray, out hitObject, 1000f, selectionMask))
+			{
+				selectedObject = hitObject.transform.gameObject;
+			}
+			else
+			{
+				selectedObject = null;
+			}
+		}
+
+		if (selectedObject != null)
+		{
+			devPointer.transform.position = selectedObject.transform.position + new Vector3(0, 2f);
+		}
+		else
+		{
+			devPointer.transform.position = new Vector3(0, -5f);
+		}
+	}
+}
