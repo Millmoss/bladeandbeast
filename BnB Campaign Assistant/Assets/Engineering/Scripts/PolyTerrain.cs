dd7e7947c84ce54816b87655f176cc2e7cd4eff5
Millmoss <omelliff@yahoo.com>
2018-09-29 12:12:31 -0400

    Working on improved terrain mesh gen

---
+++ BnB Campaign Assistant/Assets/Engineering/Scripts/PolyTerrain.cs
@@ -0,0 +1,254 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PolyTerrain : MonoBehaviour
+{
+	public Material terrainmat;
+	public int polyscale = 10;
+	public int polysize = 8;
+	public float heightscale = 2;
+	public float sizescale = 1;
+	public float perlinscale = .75f;
+	public float perlinoffset = 100;
+	public bool deform = false;
+	public float deformamount = .1f;
+	public int deformseed = 15;
+	private Vector3[,] areaMap;
+	private Vector3[,] terrainVerticesSet;
+	private Vector3[,] terrainNormalsSet;
+	private GameObject[,] polyTerrainMeshSet;
+	private int meshArrayWidth;
+	public int textureRes = 10;
+
+	void Start()
+	{
+		meshArrayWidth = Mathf.CeilToInt(polyscale / (float)(polysize + 1));
+		polyTerrainMeshSet = new GameObject[meshArrayWidth, meshArrayWidth];
+		terrainVerticesSet = new Vector3[polyscale, polyscale];
+		terrainNormalsSet = new Vector3[polyscale, polyscale];
+		areaMap = new Vector3[polyscale, polyscale];
+		for (int z = 0; z < polyscale; z++)
+		{
+			for (int x = 0; x < polyscale; x++)
+			{
+				areaMap[z, x] = new Vector3(x * sizescale, Mathf.PerlinNoise(x * perlinscale + perlinoffset, z * perlinscale + perlinoffset) * heightscale, z * sizescale);
+			}
+		}
+		for (int z = 0; z < meshArrayWidth; z++)
+		{
+			for (int x = 0; x < meshArrayWidth; x++)
+			{
+				GameObject polyTerrainMesh = new GameObject("Plane");
+				polyTerrainMesh.layer = 9;
+				polyTerrainMesh.AddComponent<MeshFilter>();
+				polyTerrainMesh.AddComponent<MeshRenderer>();
+				polyTerrainMesh.GetComponent<MeshRenderer>().material = Instantiate<Material>(terrainmat);
+				polyTerrainMeshSet[z, x] = polyTerrain(x * polysize, z * polysize, polyTerrainMesh);
+			}
+		}
+
+		for (int z = 0; z < meshArrayWidth; z++)
+		{
+			for (int x = 0; x < meshArrayWidth; x++)
+			{
+				Vector3[] normals = polyTerrainMeshSet[z, x].GetComponent<MeshFilter>().mesh.normals;
+				Vector3[] normalsDown = null;
+				if (z != 0)
+					normalsDown = polyTerrainMeshSet[z - 1, x].GetComponent<MeshFilter>().mesh.normals;
+				Vector3[] normalsUp = null;
+				if (z != meshArrayWidth - 1)
+					normalsUp = polyTerrainMeshSet[z + 1, x].GetComponent<MeshFilter>().mesh.normals;
+				Vector3[] normalsLeft = null;
+				if (x != 0)
+					normalsLeft = polyTerrainMeshSet[z, x - 1].GetComponent<MeshFilter>().mesh.normals;
+				Vector3[] normalsRight = null;
+				if (x != meshArrayWidth - 1)
+					normalsRight = polyTerrainMeshSet[z, x + 1].GetComponent<MeshFilter>().mesh.normals;
+				for (int meshz = 0; meshz <= polysize; meshz++)
+				{
+					for (int meshx = 0; meshx <= polysize; meshx++)
+					{
+						if (meshx == 0 && x != 0)
+						{
+							if (meshz == 0 && z != 0)
+							{
+
+							}
+							else if (meshz == polysize && z != meshArrayWidth - 1)
+							{
+
+							}
+							else
+							{
+
+							}
+						}
+						else if (meshx == polysize && x != meshArrayWidth - 1)
+						{
+							if (meshz == 0 && z != 0)
+							{
+
+							}
+							else if (meshz == polysize && z != meshArrayWidth - 1)
+							{
+
+							}
+							else
+							{
+
+							}
+						}
+						else if (meshz == 0 && z != 0)
+						{
+							if (meshx == 0 && x != 0)
+							{
+
+							}
+							else if (meshx == polysize && x != meshArrayWidth - 1)
+							{
+
+							}
+							else
+							{
+
+							}
+						}
+						else if (meshz == polysize && z != meshArrayWidth - 1)
+						{
+							if (meshx == 0 && x != 0)
+							{
+
+							}
+							else if (meshx == polysize && x != meshArrayWidth - 1)
+							{
+
+							}
+							else
+							{
+
+							}
+						}
+						else
+						{
+
+						}
+					}
+				}
+				polyTerrainMeshSet[z, x].GetComponent<MeshFilter>().mesh.normals = normals;
+				if (z != 0)
+					polyTerrainMeshSet[z - 1, x].GetComponent<MeshFilter>().mesh.normals = normalsDown;
+				if (z != meshArrayWidth - 1)
+					polyTerrainMeshSet[z + 1, x].GetComponent<MeshFilter>().mesh.normals = normalsUp;
+				if (x != 0)
+					polyTerrainMeshSet[z, x - 1].GetComponent<MeshFilter>().mesh.normals = normalsLeft;
+				if (x != meshArrayWidth - 1)
+					polyTerrainMeshSet[z, x + 1].GetComponent<MeshFilter>().mesh.normals = normalsRight;
+			}
+		}
+	}
+
+	void Update()
+	{
+
+	}
+
+	public GameObject polyTerrain(int xStart, int zStart, GameObject polyTerrainMesh)
+	{
+		int xEnd = xStart + polysize + 1;
+		int zEnd = zStart + polysize + 1;
+		if (xEnd > polyscale)
+			xEnd = polyscale;
+		if (zEnd > polyscale)
+			zEnd = polyscale;
+		int xPoly = xEnd - xStart;
+		int zPoly = zEnd - zStart;
+		Vector3[] terrainVertices = new Vector3[xPoly * zPoly + (xPoly - 1) * (zPoly - 1)];    //areamap in the first polyscale square, the inbetweens in the second
+		int[] terrainTriangles = new int[(xPoly - 1) * (zPoly - 1) * 12];
+
+		for (int z = 0; z < zPoly; z++)
+		{
+			for (int x = 0; x < xPoly; x++)
+			{
+				terrainVertices[z * xPoly + x] = areaMap[x + xStart, z + zStart];
+			}
+		}
+
+		for (int z = 0; z < zPoly - 1; z++)
+		{
+			for (int x = 0; x < xPoly - 1; x++)
+			{
+				float y = (areaMap[x + xStart, z + zStart].y + areaMap[x + 1 + xStart, z + zStart].y + areaMap[x + xStart, z + 1 + zStart].y + areaMap[x + 1 + xStart, z + 1 + zStart].y) / 4;
+				terrainVertices[xPoly * zPoly + ((xPoly - 1) * z) + x] = new Vector3(areaMap[x + xStart, z + zStart].x + .5f * sizescale, y, areaMap[x + xStart, z + zStart].z + .5f * sizescale);
+			}
+		}
+
+		int cur = 0;
+		for (int z = 0; z < zPoly - 1; z++)
+		{
+			for (int x = 0; x < xPoly - 1; x++)
+			{
+				//setting triangles
+				terrainTriangles[cur + 2] = z * xPoly + x;
+				terrainTriangles[cur + 1] = xPoly * zPoly + ((xPoly - 1) * z) + x;
+				terrainTriangles[cur + 0] = z * xPoly + x + 1;
+				terrainTriangles[cur + 3] = xPoly * zPoly + ((xPoly - 1) * z) + x;
+				terrainTriangles[cur + 4] = z * xPoly + x + 1;
+				terrainTriangles[cur + 5] = (z + 1) * xPoly + x + 1;
+				terrainTriangles[cur + 8] = z * xPoly + x;
+				terrainTriangles[cur + 7] = (z + 1) * xPoly + x;
+				terrainTriangles[cur + 6] = xPoly * zPoly + ((xPoly - 1) * z) + x;
+				terrainTriangles[cur + 9] = (z + 1) * xPoly + x;
+				terrainTriangles[cur + 10] = xPoly * zPoly + ((xPoly - 1) * z) + x;
+				terrainTriangles[cur + 11] = (z + 1) * xPoly + x + 1;
+				//increment
+				cur += 12;
+			}
+		}
+
+		polyTerrainMesh.GetComponent<MeshFilter>().mesh.vertices = terrainVertices;
+		polyTerrainMesh.GetComponent<MeshFilter>().mesh.triangles = terrainTriangles;
+		polyTerrainMesh.GetComponent<MeshFilter>().mesh.RecalculateNormals();
+
+		Vector2[] terrainUV = new Vector2[terrainVertices.Length];
+
+		for (int z = 0; z < zPoly; z++)
+		{
+			for (int x = 0; x < xPoly; x++)
+			{
+				terrainUV[z * xPoly + x] = new Vector2(x / (float)(xPoly - 1), z / (float)(zPoly - 1));
+			}
+		}
+
+		for (int z = 0; z < zPoly - 1; z++)
+		{
+			for (int x = 0; x < xPoly - 1; x++)
+			{
+				terrainUV[xPoly * zPoly + ((xPoly - 1) * z) + x] = new Vector2((x + .5f) / (xPoly - 1), (z + .5f) / (zPoly - 1));
+			}
+		}
+
+		polyTerrainMesh.GetComponent<MeshFilter>().mesh.uv = terrainUV;
+
+		Texture2D terrainTexture = new Texture2D(xPoly * textureRes, zPoly * textureRes, TextureFormat.ARGB32, false);
+
+		Color gr = Color.green;
+		gr = new Color(gr.r, gr.g, gr.b);
+
+		for (int z = 0; z < zPoly * textureRes; z++)
+		{
+			for (int x = 0; x < xPoly * textureRes; x++)
+			{
+				float y = Mathf.PerlinNoise((z / (float)textureRes + zStart) * perlinscale + perlinoffset, (x / (float)textureRes + xStart) * perlinscale + perlinoffset);
+				terrainTexture.SetPixel(x, z, new Color(gr.r * (1f * y), gr.g * (1f * y), gr.b * (1f * y)));
+			}
+		}
+		terrainTexture.Apply();
+		terrainTexture.filterMode = FilterMode.Point;
+
+		polyTerrainMesh.GetComponent<MeshRenderer>().material.mainTexture = terrainTexture;
+
+		return polyTerrainMesh;
+	}
+}
